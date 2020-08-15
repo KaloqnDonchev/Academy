24db0bfae6853d564788b25fb17224252a97a63b
Kaloyan <kaloqndonchev99@gmail.com>
2020-08-15 21:59:33 +0300

    Task Third largest number in array

---
+++ Javascript/thirdLargest/thirdLargest.js
@@ -0,0 +1,19 @@
+var button = document.getElementById('button');
+button.addEventListener('click', function () {
+    var stringText = document.getElementById('input').value;
+    var stringArray = stringText.split(",");
+    var array = stringArray.map((element) => {
+        if (isNaN(element)) {
+            document.getElementById('fill').innerHTML = "Wrong input";
+            throw new Error();
+            
+        } else {
+            return parseInt(element, 10)
+        }
+    })
+    var max = Math.max.apply(null, array);
+    array.splice(array.indexOf(max), 1);
+    Math.max.apply(null, array);
+    array.splice(array.indexOf(max), 1);
+    document.getElementById('fill').innerHTML = Math.max.apply(null, array);
+})
